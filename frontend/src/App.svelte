53ba84c21c5fa64e5cd6de76d10d7af71ab87832
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-18 14:04:01 +1000

    add new paths

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
     import { Router, Route } from "svelte-routing";
     import StorePage from "./pages/store/StorePage.svelte";
+    import RegPage from "./pages/reg/RegPage.svelte";
+    import AuthPage from "./pages/auth/AuthPage.svelte";
     import Header from "./components/Header.svelte";
     import Footer from "./components/Footer.svelte";
 </script>
@@ -11,6 +13,12 @@
     <main>
         <Route path="/">
             <StorePage/>
+        </Route>
+        <Route path="/regestration">
+            <RegPage/>
+        </Route>
+        <Route path="/authorization">
+            <AuthPage/>
         </Route>
         <Route path="/profile">
             <StorePage/>
