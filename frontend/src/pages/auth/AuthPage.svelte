94fad14abbae36a36b7ab0e976133981076175ca
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-18 18:39:21 +1000

    add auth page

---
+++ frontend/src/pages/auth/AuthPage.svelte
@@ -0,0 +1,62 @@
+<script>
+    import { Link } from "svelte-routing";
+    import Button from "../../components/Button.svelte";
+    
+    let maxlength = 30;
+</script>
+
+
+<div class="container">
+    <form>
+        <input type="text" placeholder="Login" {maxlength}>
+        <input type="password" placeholder="Password" {maxlength}>
+        <Link to="reg">
+            <div class="auth-button">Don't have an account?</div>
+        </Link>
+        <Link to="/">
+            <Button type="accent" size="big" text="Login" />
+        </Link>
+    </form>
+</div>
+
+
+<style>
+    .container {
+        height: 90vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-bottom: 100px;
+    }
+
+    input {
+        width: 300px;
+        font-size: 14px;
+        padding: 12px 18px;
+        border-radius: 8px;
+        border: 2px solid var(--color-neutral-2);
+        margin: 6px;
+    }
+
+    input:focus {
+        border: 2px solid var(--color-primary);
+    }
+
+    .auth-button {
+        font-size: 14px;
+        margin: 14px;
+        color: var(--color-neutral-3);
+        text-decoration: underline;
+    }
+    
+    .auth-button:hover {
+        color: var(--color-primary);
+    }
+</style>
