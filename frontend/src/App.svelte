7c5e79811f445a2bf2f158abf7444b54ceb25318
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-19 21:26:19 +1000

    add path for profile page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,11 @@
 <script>
     import { Router, Route } from "svelte-routing";
-    import StorePage from "./pages/store/StorePage.svelte";
     import RegPage from "./pages/reg/RegPage.svelte";
     import AuthPage from "./pages/auth/AuthPage.svelte";
-    import ProductPage from "./pages/product/ProductPage.svelte";
+    import StorePage from "./pages/store/StorePage.svelte";
     import UploadPage from "./pages/upload/UploadPage.svelte";
+    import ProductPage from "./pages/product/ProductPage.svelte";
+    import ProfilePage from "./pages/profile/ProfilePage.svelte";
     import Header from "./components/Header.svelte";
     import Footer from "./components/Footer.svelte";
 </script>
@@ -17,6 +18,7 @@
         <Route path="/reg" component={RegPage} />
         <Route path="/auth" component={AuthPage} />
         <Route path="/upload" component={UploadPage} />
+        <Route path="/profile" component={ProfilePage} />
         <Route path="/store/:id" component={ProductPage} />
     </main>
     <Footer/>
