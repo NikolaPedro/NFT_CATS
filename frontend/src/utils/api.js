5885edef62d4ac6a5bbf64107961160aba261400
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-20 22:57:32 +1000

    add export API_HOST

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_HOST = "http://127.0.0.1:5000";
+export const API_HOST = "http://127.0.0.1:5000";
 
 export function getProductData(id) {
     return fetch(`${API_HOST}/store/page=${id}`).json();
