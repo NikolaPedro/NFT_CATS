3b088466e78b22972507bc2bd8f33ee781bff270
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-21 20:49:09 +1000

    add fetch for product count

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,3 +3,8 @@ export const API_HOST = "http://127.0.0.1:5000";
 export function getProductData(id) {
     return fetch(`${API_HOST}/store/page=${id}`).json();
 }
+
+export function getProductsCount() {
+    const { answer } = fetch(`${API_HOST}/store/count`).json();
+    return answer;
+}
