3632a49f6e510f14bb0ba6ebddd0d44f231bfeda
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-21 20:47:26 +1000

    rework product list

---
+++ frontend/src/components/ProductList.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount, onDestroy, debug } from "svelte/internal";
+    import { getProductsCount } from "../utils/api";
+    import ProductCard from "./ProductCard.svelte";
+
+    let listElement;
+    $: products = [];
+    let maxCount = 0;
+
+    onMount(async () => {
+        maxCount = await getProductsCount();
+        window.addEventListener("scroll", function () {
+            let scrollY = listElement.offsetTop + listElement.clientHeight;
+            if (scrollY > window.scrollY && products.length < maxCount) {
+                products = [...products, products.length + 1]
+            }
+        });
+    });
+
+    onDestroy(() => {
+        window.removeEventListener("scroll");
+    });
+</script>
+
+
+<div bind:this={listElement}>
+    {#each products as index}
+        <ProductCard id={index} />
+    {/each}
+</div>
+
+
+<style>
+    div {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 30px;
+    }
+</style>
